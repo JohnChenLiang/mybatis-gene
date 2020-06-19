478273db9f9fd8038f4b8606f7de8c0590b36a3e
陈梁 <759670407@qq.com>
2020-06-19 11:00:50 +0800

    1.test

--- src/main/java/com/ivorymanager/util/GeneratorSqlUtil.java
+++ src/main/java/com/ivorymanager/util/GeneratorSqlUtil.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 public class GeneratorSqlUtil {
 
-
     public static void main(String[] args) {
         try {
             List<String> warnings = new ArrayList<String>();
